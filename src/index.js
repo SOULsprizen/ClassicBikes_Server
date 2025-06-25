67f3aba0476e702c9d58b36db92c9216c537aec8
ravi singh <ravisingh007ravi@gmail.com>
2025-06-25 07:35:56 +0530

    dsfsd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const router = require('./routes/routes.js');
 
 dotenv.config();
 const app = express();
 
 app.use(express.json())
+app.use(cors());
 
 const PORTNUMBER = 8080;
 
